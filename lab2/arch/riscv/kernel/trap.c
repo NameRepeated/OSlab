ca4ebf545143fe57b0e16f8d3b6632c313b3b176
NameRepeated <2603939740@qq.com>
2022-10-21 12:32:25 +0800

    lab2

---
+++ lab2/arch/riscv/kernel/trap.c
@@ -0,0 +1,29 @@
+#include "trap.h"
+#include "types.h"
+#include "clock.h"
+#include "printk.h"
+
+
+void handler_interrupt(unsigned long scause, unsigned long sepc,unsigned long regs) 
+{
+    switch (scause & ~TRAP_MASK) {
+        case STI:
+            printk("[S] Supervisor Mode Timer Interrupt\n");
+            clock_set_next_event();
+            break;
+        default:
+            break;
+    }
+}
+
+void handler_exception(unsigned long scause, unsigned long sepc,unsigned long regs) 
+{
+}
+
+void trap_handler(unsigned long scause, unsigned long sepc,unsigned long regs) 
+{
+    if (scause & TRAP_MASK)                    // #define TRAP_MASK (1UL << 63)
+        handler_interrupt(scause, sepc,regs);
+    else
+        handler_exception(scause, sepc,regs);
+}
